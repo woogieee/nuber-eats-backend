60943867a4ccddda40058e5a2404672cc397a4d7
jay <p0425@nate.com>
2023-12-07 18:00:16 +0900

    1. 오더 접수 기능 완료 2. ID로 오더 가져오기 제작 중

--- src/orders/orders.resolver.ts
+++ src/orders/orders.resolver.ts
@@ -1,15 +1,17 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { Order } from './entities/order.entity';
 import { OrderService } from './orders.service';
 import { CreateOrderInput, CreateOrderOutput } from './dtos/create-order.dto';
 import { AuthUser } from 'src/auth/auth-user.decorator';
 import { User } from 'src/users/entities/user.entity';
 import { Role } from 'src/auth/role.decorator';
+import { GetOrdersInput, GetOrdersOutput } from './dtos/get-orders.dto';
 
 @Resolver(() => Order)
 export class OrderResolver {
   constructor(private readonly ordersService: OrderService) {}
 
+  // 오더 생성
   @Mutation(() => CreateOrderOutput)
   @Role(['Client'])
   async createOrder(
@@ -17,5 +19,15 @@ export class OrderResolver {
     @Args('input') createOrderInput: CreateOrderInput,
   ): Promise<CreateOrderOutput> {
     return this.ordersService.createOrder(customer, createOrderInput);
+  }
+
+  // 오더 가져오기
+  @Query(() => GetOrdersOutput)
+  @Role(['Any'])
+  async getOrders(
+    @AuthUser() user: User,
+    @Args('input') getOrdersInput: GetOrdersInput,
+  ): Promise<GetOrdersOutput> {
+    return this.ordersService.getOrders(user, getOrdersInput);
   }
 }
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -4,9 +4,10 @@ import { Order } from './entities/order.entity';
 import { Repository } from 'typeorm';
 import { CreateOrderInput, CreateOrderOutput } from './dtos/create-order.dto';
 import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
-import { User } from 'src/users/entities/user.entity';
+import { User, UserRole } from 'src/users/entities/user.entity';
 import { OrderItem } from './entities/order-item.entity';
 import { Dish } from 'src/restaurants/entities/dish.entity';
+import { GetOrdersInput, GetOrdersOutput } from './dtos/get-orders.dto';
 
 @Injectable()
 export class OrderService {
@@ -26,57 +27,105 @@ export class OrderService {
     customer: User,
     { restaurantId, items }: CreateOrderInput,
   ): Promise<CreateOrderOutput> {
-    const restaurant = await this.restaurants.findOne({
-      where: { id: restaurantId },
-    });
-    if (!restaurant) {
-      return {
-        ok: false,
-        error: 'Restaurant not found',
-      };
-    }
-    for (const item of items) {
-      const dish = await this.dishes.findOne({ where: { id: item.dishId } });
-      // const dish = await this.dishes.findOne({ where: { id: 666 } });
-      if (!dish) {
+    try {
+      const restaurant = await this.restaurants.findOne({
+        where: { id: restaurantId },
+      });
+      if (!restaurant) {
         return {
           ok: false,
-          error: 'Dish not found',
+          error: 'Restaurant not found',
         };
       }
-      console.log(`Dish price: ${dish.price}`);
-      for (const itemOption of item.options) {
-        const dishOption = dish.options.find(
-          (dishOption) => dishOption.name === itemOption.name,
-        );
-        if (dishOption) {
-          if (dishOption.extra) {
-            console.log(`$USD + ${dishOption.extra}`);
-          } else {
-            const dishOptionChoice = dishOption.choices.find(
-              (optionChoice) => optionChoice.name === itemOption.choice,
-            );
-            if (dishOptionChoice) {
-              if (dishOptionChoice.extra) {
-                console.log(`$USD + ${dishOptionChoice.extra}`);
+      // 모든 dish의 총 합계
+      let orderFinalPrice = 0;
+      const orderItems: OrderItem[] = [];
+      for (const item of items) {
+        const dish = await this.dishes.findOne({ where: { id: item.dishId } });
+        // const dish = await this.dishes.findOne({ where: { id: 666 } });
+        if (!dish) {
+          return {
+            ok: false,
+            error: 'Dish not found',
+          };
+        }
+        // 각 dish 최종가격 계산
+        let dishFinalPrice = dish.price;
+        console.log(`Dish price: ${dish.price}`);
+        // item의 옵션 계산
+        for (const itemOption of item.options) {
+          const dishOption = dish.options.find(
+            (dishOption) => dishOption.name === itemOption.name,
+          );
+          if (dishOption) {
+            if (dishOption.extra) {
+              dishFinalPrice = dishFinalPrice + dishOption.extra;
+            } else {
+              const dishOptionChoice = dishOption.choices.find(
+                (optionChoice) => optionChoice.name === itemOption.choice,
+              );
+              if (dishOptionChoice) {
+                if (dishOptionChoice.extra) {
+                  dishFinalPrice = dishFinalPrice + dishOptionChoice.extra;
+                }
               }
             }
           }
         }
+        orderFinalPrice = orderFinalPrice + dishFinalPrice;
+        const orderItem = await this.orderItems.save(
+          this.orderItems.create({
+            dish,
+            options: item.options,
+          }),
+        );
+        orderItems.push(orderItem);
       }
-      /*await this.orderItems.save(
-        this.orderItems.create({
-          dish,
-          options: item.options,
+      await this.orders.save(
+        this.orders.create({
+          customer,
+          restaurant,
+          total: orderFinalPrice,
+          items: orderItems,
         }),
-      );*/
+      );
+      return {
+        ok: true,
+      };
+    } catch {
+      return {
+        ok: false,
+        error: 'Could not create order',
+      };
     }
-    /* const order = await this.orders.save(
-      this.orders.create({
-        customer,
-        restaurant,
-      }),
-    );
-    console.log(order); */
+  }
+
+  // ID로 오더 가져오기
+  async getOrders(
+    user: User,
+    { status }: GetOrdersInput,
+  ): Promise<GetOrdersOutput> {
+    // console.log(user);
+    if (user.role === UserRole.Client) {
+      const orders = await this.orders.find({
+        // where: { customer: { user.customer } },
+        where: { customer: true },
+      });
+    } else if (user.role === UserRole.Delivery) {
+      const orders = await this.orders.find({
+        // where: { driver: { orders: user } },
+        where: { driver: true },
+      });
+    } else if (user.role === UserRole.Owner) {
+      const orders = await this.restaurants.find({
+        // where: { owner: { orders: user } },
+        where: { owner: true },
+        relations: ['orders'],
+      });
+      console.log(orders);
+    }
+    return {
+      ok: false,
+    };
   }
 }
